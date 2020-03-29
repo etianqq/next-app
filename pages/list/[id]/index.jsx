5cd20420e9aa15798839b8485c64a0c71b48d174
Yelan <yelan@stonefin.cn>
2020-03-29 11:45:28 +0800

    update

---
+++ pages/list/[id]/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Item() {
+    return (
+      <p>This is item</p>
+    )
+  }
+  
+  export default Item
---
+++ pages/list/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+
+function List() {
+
+    function renderList() {
+        for (let i = 0; i < 10; i++) {
+            const href = `/list/${i}`
+            return (<li key={i}>
+                <Link href={href}>
+                    <a>item {i}</a>
+                </Link></li>)
+        }
+    }
+
+    return (
+        <ul>
+            {renderList()}
+        </ul>
+    )
+}
+
+export default List
